44f8f19099a0cefa28e8b72d1ebd2d988997a633
Hossamabohadima <147110912+Hossamabohadima@users.noreply.github.com>
2023-12-14 13:35:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,21 @@
 
 int main()
 {
-    Board *X_o = new X_O_Board;
+
+    int choice;
     Player *players[2];
-    players[0] = new Player(1, 'X');
-    players[1] = new RandomPlayer('O');
-    GameManager v(X_o, players);
-    v.run();
-    // delete[] players;
-    // delete X_o;
+    players[0] = new Player(1, 'x');
+
+    cout << "Welcome to FCAI X-O Game. :)\n";
+    cout << "Press 1 if you want to play with computer: ";
+    cin >> choice;
+    if (choice != 1)
+        players[1] = new Player(2, 'o');
+    else
+        // Player pointer points to child
+        players[1] = new RandomPlayer('o', 3);
+
+    GameManager x_o_game(new X_O_Board, players);
+    x_o_game.run();
+    system("pause");
 }
